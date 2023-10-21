b2d5a6846a9cab0c7f9ec5997f4751c9a9952f5e
Tiago Sampaio <tiagofssampaio@gmail.com>
2023-10-21 13:23:49 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     build: {
         rollupOptions: {
-            external: ['vue']
+            external: ['vue', 'vue-router']
         }
     }
 })

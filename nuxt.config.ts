4f70e8d1067d62fd50555b6e09e0fc9ea44d6e91
Tiago Sampaio <tiagofssampaio@gmail.com>
2023-10-23 20:26:20 +0100

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     experimental: {
-        appManifest: false
+        appManifest: false,
     },
     supabase : {
         redirect: false,

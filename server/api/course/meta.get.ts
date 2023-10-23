c33737e0ff305b185109c14d90fb8623b1b9557d
Tiago Sampaio <tiagofssampaio@gmail.com>
2023-10-23 21:05:42 +0100

    fix meta endpoint

--- server/api/course/meta.get.ts
+++ server/api/course/meta.get.ts
@@ -7,27 +7,69 @@ const lessonSelect = Prisma.validator<Prisma.LessonArgs>()({
         title: true,
         slug: true,
         number: true,
-    }
+    },
 });
-export type LessonOutline = Prisma.LessonGetPayload<typeof lessonSelect>;
+export type LessonOutline = Prisma.LessonGetPayload<
+    typeof lessonSelect
+    > & {
+    path: string;
+};
 
-const chapterSelect = Prisma.validator<Prisma.ChapterArgs>()({
-    select: {
-        title: true,
-        slug: true,
-        number: true,
-        lessons: lessonSelect,
-    }
-});
-export type ChapterOutline = Prisma.ChapterGetPayload<typeof chapterSelect>;
+const chapterSelect =
+    Prisma.validator<Prisma.ChapterArgs>()({
+        select: {
+            title: true,
+            slug: true,
+            number: true,
+            lessons: lessonSelect,
+        },
+    });
+export type ChapterOutline = Omit<
+    Prisma.ChapterGetPayload<typeof chapterSelect>,
+    'lessons'
+    > & {
+    lessons: LessonOutline[];
+};
 
 const courseSelect = Prisma.validator<Prisma.CourseArgs>()({
     select: {
         title: true,
         chapters: chapterSelect,
-    }
+    },
 });
-export type CourseOutline = Prisma.CourseGetPayload<typeof courseSelect>;
+export type CourseOutline = Omit<
+    Prisma.CourseGetPayload<typeof courseSelect>,
+    'chapters'
+    > & {
+    chapters: ChapterOutline[];
+};
 
-export default async () => prisma.course.findFirst(courseSelect);
+export default defineEventHandler(
+    async (): Promise<CourseOutline> => {
+        const outline = await prisma.course.findFirst(
+            courseSelect
+        );
 
+        // Error if there is no course
+        if (!outline) {
+            throw createError({
+                statusCode: 404,
+                statusMessage: 'Course not found',
+            });
+        }
+
+        // Map the outline so we can add a path to each lesson
+        const chapters = outline.chapters.map((chapter) => ({
+            ...chapter,
+            lessons: chapter.lessons.map((lesson) => ({
+                ...lesson,
+                path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+            })),
+        }));
+
+        return {
+            ...outline,
+            chapters,
+        };
+    }
+);

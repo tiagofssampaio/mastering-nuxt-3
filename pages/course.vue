994b96235e7643adc2432b151db1323d3e98efe2
Tiago Sampaio <tiagofssampaio@gmail.com>
2023-10-23 17:26:29 +0100

    API; refactor

--- pages/course.vue
+++ pages/course.vue
@@ -4,7 +4,7 @@
       <h1>
         <span class="font-medium">
           Course:
-          <span class="font-bold">{{ title }}</span>
+          <span class="font-bold">{{ course.title }}</span>
         </span>
       </h1>
       <UserCard />
@@ -15,7 +15,7 @@
           class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
       >
         <h3>Chapters</h3>
-        <div class="space-y-1 mb-4 flex flex-col" v-for="chapter in chapters" :key="chapter.slug">
+        <div class="space-y-1 mb-4 flex flex-col" v-for="chapter in course.chapters" :key="chapter.slug">
           <h4>{{ chapter.title }}</h4>
           <NuxtLink
               v-for="(lesson, index) in chapter.lessons"
@@ -57,12 +57,11 @@
 </template>
 
 <script setup>
-const { chapters, title } = useCourse();
+const course = await useCourse();
+const firstLesson = await useFirstLesson();
 
 const resetError = async (error) => {
-  await navigateTo(
-      '/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3'
-  );
+  await navigateTo(firstLesson.path);
   error.value = null;
 }
 </script>
---
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -0,0 +1,36 @@
+import { Lesson, LessonWithPath, Chapter, Course} from '~/types/course';
+import course from '~/server/courseData';
+
+course as Course;
+
+export default defineEventHandler((event): LessonWithPath => {
+    // @ts-ignore
+    const { chapterSlug, lessonSlug } = event.context.params;
+
+    const chapter: Maybe<Chapter> = course.chapters.find(
+        (chapter) => chapter.slug === chapterSlug
+    );
+
+    if(!chapter) {
+        throw createError({
+            statusCode: 404,
+            message: 'Chapter not found',
+        });
+    }
+
+    const lesson: Maybe<Lesson> = chapter.lessons.find(
+        (lesson) => lesson.slug === lessonSlug
+    );
+
+    if(!lesson) {
+        throw createError({
+            statusCode: 404,
+            message: 'Lesson not found',
+        });
+    }
+
+    return {
+        ...lesson,
+        path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+    };
+});

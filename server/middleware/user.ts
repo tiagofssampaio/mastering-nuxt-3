423fef6cfe0249ab03e9d211139f8ffad8fa5971
Tiago Sampaio <tiagofssampaio@gmail.com>
2023-10-24 10:19:23 +0100

    Protect API routes with auth

--- server/middleware/user.ts
+++ server/middleware/user.ts
@@ -2,6 +2,7 @@ import { serverSupabaseUser } from '#supabase/server';
 
 export default defineEventHandler(async (event) => {
    let user;
+   // Use a try catch. The catch will be triggered if the user is not logged in
    try {
       user = await serverSupabaseUser(event);
    } catch(error) {
